af37d0884d9f6d1172c013fd4baa2c711dca4e2c
gloiremao <gloiremao@gmail.com>
2015-03-08 01:44:58 +0800

    Modulation
    Big change

--- src/Video.java
+++ src/Video.java
@@ -6,10 +6,22 @@ public class Video {
 	public String height;
 	public String frame;
 	public String biterate;
+	public double duration;
+	public String size;
+	public double psnr;
+	public String type;
 	
 	public void setResolution(String w,String h){
 		this.width = w;
 		this.height = h;
+	}
+	
+	public String getMetadata(){
+		return "Video:"+this.filename+" "+this.width+"x"+this.height+" Duration"+this.duration+" bitrate:"+this.biterate+" size:"+this.size+" original psnr:"+this.psnr;
+	}
+	
+	public String getInfo(){
+		return this.filename +"\t"+this.duration+"\t"+this.biterate+"\t"+this.size+"\tn"+"\t"+this.psnr;
 	}
 	
 }
